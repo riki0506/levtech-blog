442cd6dc583cd7cdfc92e63f129983ac9382a822
riki0506 <rikinakajima0506@gmail.com>
2023-12-05 12:15:20 +0900

    change paginate

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,7 +20,7 @@ class Post extends Model
         'body'
         ];
         
-    public function getPaginateByLimit(int $limit_count = 2)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
     // updated_atで降順に並べたあと、limitで件数制限をかける
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
