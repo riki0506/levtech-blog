4db1d7f4c4e39df0fe8bfb1a88ef82e6207ba620
riki0506 <rikinakajima0506@gmail.com>
2023-12-13 11:58:09 +0900

    09-3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
+    <!--<head>-->
+    <!--    <meta charset="utf-8">-->
+    <!--    <title>Blog</title>-->
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
+    <!--    <link href="https://fonts.googleapis.com/css?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">-->
+    <!--</head>-->
+    <x-app-layout>
+    <x-slot name="header">
+        Index
+    </x-slot>
     <body class="antialiased">
         <h1>Blog Name</h1>
         <a href="/posts/create">create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
+                    <br>
                     <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
                     <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
@@ -35,5 +40,8 @@
                 }
             }
         </script>
+        <br>
+        {{ Auth::user()->name }}
     </body>
+    </x-app-layout>
 </html>
